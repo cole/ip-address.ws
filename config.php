9ab5eee4e2316b7dbc64a2edce5222ae34d8522b
Cole Maclean <cole@warpmail.net>
2010-12-10 15:43:23 +0000

    Fix typo

--- config.php
+++ config.php
@@ -6,7 +6,7 @@
 define("ANALYTICS_CODE", "UA-292915-3"); // Replace this with your analytics code
 define("TEMPLATEPATH","templates");
 define("STATICPATH","static");
-define("VERSION","0.5")
+define("VERSION","0.5");
 define("STATICCACHETIME","1209600"); // 2 weeks
 define("CACHETIME","60");
 define("DEBUG",false);
